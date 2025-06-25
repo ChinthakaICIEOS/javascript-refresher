536814916eb3a01ff5e953f4bb7e003006c255bc
ChinthakaICIEOS <chinthaka@icieos.com>
2025-06-25 17:24:38 +0530

    what is the callback ???

--- play.js
+++ play.js
@@ -1,7 +1,16 @@
 // Async code
+const fetchData = callback => {
+    setTimeout(() => {
+        callback("Done!");
+    },1500);
+};
+
 setTimeout(() => {
-    console.log("Hey Async...");
-},1000)
+    console.log("Timer is done!");
+    fetchData(text => {
+        console.log(text);
+    });
+},2000);
 
 // Sync code
 console.log("Hey Sync 01");
