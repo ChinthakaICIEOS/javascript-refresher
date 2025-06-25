83d828dc30066b646a24b0de19aef892cecefec0
ChinthakaICIEOS <chinthaka@icieos.com>
2025-06-25 14:41:47 +0530

    anonymous function refer to the object

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
-let name_ = "Chinthaka";
-let age = 24;
-let isMarried = true;
-
-const userFunction = (userName,userAge,userMarried) => {
-    return (
-        "I am " + 
-        userName + 
-        ", I am " + 
-        userAge + 
-        " and Married is " +
-         userMarried
-    );
+const user = {
+    name : "Chinthaka",
+    age : 24,
+    greet : function(){
+        console.log("I am " + this.name);
+    }
 }
 
-const getSum = (a,b) => a + b;
-
-const addOne = a => a + 1;
-
-console.log(userFunction(name_,age,isMarried));
-console.log(getSum(3,4));
-console.log(addOne(7));
+console.log(user);
+user.greet();
